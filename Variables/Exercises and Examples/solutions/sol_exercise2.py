77bd38e5dc891be7cea2efc014d7a3c4ad837fa1
Madelyn Forrester <forrestermadelyn@gmail.com>
2020-06-26 16:30:26 +0000

    added solutions to exercises

---
+++ Variables/Exercises and Examples/solutions/sol_exercise2.py
@@ -0,0 +1,36 @@
+"""
+WELCOME TO VARIABLES EXERCISE 1
+
+BEFORE YOU DO ANY CODING COPY THE BELOW TEXT INTO THE .replit FILE
+
+language = "python3"
+run = "cd Variables; cd 'Exercises and Examples'; cd solutions; python3 exercise2.py"
+
+For examples please refer to the example2.py file
+
+Start with the base code below 
+
+combine var1 and var2 to print out True
+
+"""
+
+""" BASE CODE STARTS HERE """
+
+var1 = False
+var2 = False
+
+""" YOUR CODE STARTS HERE """
+
+var3 = (var1 == var2)
+
+# There are many other solutions problem this is one
+
+""" CHECK YOUR RESULTS - DO NOT CHANGE CODE BELOW """
+print("var3: " + var3)
+
+""" 
+This should print out: 
+
+var3: True
+
+"""

43abb07920b8bff8e2453dd519a77858627a77ab
Madelyn Forrester <forrestermadelyn@gmail.com>
2020-06-26 19:42:22 +0000

    exercises done for lists

--- Lists_IO_Random/Exercises and Examples/exercise2.py
+++ Lists_IO_Random/Exercises and Examples/exercise2.py
@@ -10,18 +10,18 @@ For examples please refer to the example files
 
 Start with the base code below 
 
+GOAL: Write to a file called “RandomNumbers.txt” the following text: “Today’s lucky number is “ followed by a random number
 
 """
 
 """ BASE CODE STARTS HERE - DO NOT CHANGE CODE BELOW """
-
-
+import random
+file_path = "./text_files/RandomNumbers.txt"
 
 """ YOUR CODE STARTS HERE """
 
 
 
 """ 
-This should print out: 
-
+This should not print out anything but instead should create a file in the text_files directory. 
 """
---
+++ Lists_IO_Random/Exercises and Examples/solutions/sol_exercise2.py
@@ -0,0 +1,32 @@
+"""
+WELCOME TO LISTS_IO_RANDOM EXERCISE 2
+
+BEFORE YOU DO ANY CODING COPY THE BELOW TEXT INTO THE .replit FILE
+
+language = "python3"
+run = "cd Lists_IO_Random; cd 'Exercises and Examples'; cd solutions; python3 sol_exercise2.py"
+
+For examples please refer to the example files
+
+Start with the base code below 
+
+GOAL: Write to a file called "RandomNumbers.txt" the following text: 'Today’s lucky number is ' followed by a random number
+
+"""
+
+""" BASE CODE STARTS HERE - DO NOT CHANGE CODE BELOW """
+import random
+
+file_path = "../text_files/RandomNumbers.txt"
+
+""" YOUR CODE STARTS HERE """
+
+f = open(file_path, "w")
+r = random.randrange(0,100,1)
+contents =  'Today’s lucky number is ' + str(r)
+f.write(contents)
+f.close()
+
+""" 
+This should not print out anything but instead should create a file in the text_files directory. 
+"""

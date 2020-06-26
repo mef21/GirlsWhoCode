55299cb3257e591d38e7e8ec36335b8d368036f7
Madelyn  Forrester <forrestermadelyn@gmail.com>
2020-06-26 15:30:41 +0000

    "Added Variable Exercises"

---
+++ Variables/exercise1/example1.py
@@ -0,0 +1,51 @@
+"""
+WELCOME TO VARIABLES EXAMPLE 1
+
+BEFORE YOU DO ANY CODING COPY THE BELOW TEXT INTO THE .replit FILE
+
+language = "python3"
+run = "cd Variables; cd exercise1; python3 example1.py"
+
+
+Below are a series of examples using different types of variables and how to manipulate them: 
+
+"""
+
+""" STRING EXAMPLE
+
+Print out the value of a_string with other text such that the output is - hello world! Have a great day! Python is cool.
+
+"""
+# Solution 1
+a_string = "hello world"
+print(a_string + "! Have a great day! Python is cool.")
+
+# Solution 2
+a_string = "hello world"
+a_string = a_string + "! Have a great day! Python is cool."
+print(a_string) 
+
+# Solution 3
+a_string = "hello world"
+b_string = "! Have a great day! Python is cool."
+print(a_string + b_string)
+
+""" INTEGER/FLOAT EXAMPLE 
+
+Add multiply and divide to print out - number: 10
+
+""" 
+
+# Solution 1 
+num1 = 2
+num2 = 10
+num3 = (num1*num2)/2
+print("number: " + str(num3))
+
+# Solution 2 
+
+num1 = 5
+num2 = 4
+num3 = 2
+num4 = (num2/num3)*num1
+print("number: " + str(num4))
---
+++ Variables/exercise1/exercise1.py
@@ -0,0 +1,43 @@
+"""
+WELCOME TO VARIABLES EXERCISE 1
+
+BEFORE YOU DO ANY CODING COPY THE BELOW TEXT INTO THE .replit FILE
+
+language = "python3"
+run = "cd Variables; cd exercise1; python3 exercise1.py"
+
+
+Start with the base code below 
+
+Example:  
+
+print out the value of a_string with other text such that the output is: 
+  hello world! Have a great day! Python is cool.
+
+Try to: 
+
+
+"""
+a_string = "hello world"
+print(a_string + "Have a great day! Python is cool.")
+
+""" BASE CODE STARTS HERE """
+
+color1 = "red"
+color2 = "blue"
+
+""" YOUR CODE STARTS HERE """
+
+
+""" CHECK YOUR RESULTS - DO NOT CHANGE CODE BELOW """
+print("color1: " + color1)
+print("color2: " + color2)
+
+
+""" 
+This should print out: 
+
+color1: blue
+color2: red
+
+"""

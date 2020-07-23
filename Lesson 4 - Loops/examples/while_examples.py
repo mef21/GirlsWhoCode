a4e537efba5a33bb16a014302703ce3e806dabac
Madelyn Forrester <forrestermadelyn@gmail.com>
2020-07-23 15:44:55 +0000

    added while examples

--- Lesson 4 - Loops/examples/while_examples.py
+++ Lesson 4 - Loops/examples/while_examples.py
@@ -19,3 +19,38 @@ while(is_val != val):
 
 print("Finished executing the loop")
 
+""" EXAMPLE 2
+Sum up all of the numbers from 1 to 100
+"""
+sum1 = 0 
+# this loop goes through all values of i from 1 to 100
+i = 1
+# also can use while i <= 100
+while i < 101:
+  # also can use sum1 = sum1 + i
+  sum1 += i
+  i += 1
+print(sum1)
+
+""" EXAMPLE 3
+Print out all entires in an array on different lines
+"""
+arr = ['hello','my','name','is','mr.','duck']
+i = 0 
+while i < len(arr):
+  print(arr[i])
+  i += 1
+
+""" EXAMPLE 4
+Print out all entires in an array on the same line
+"""
+
+arr = ['hello','my','name','is','Mr.','duck']
+total1 = ""
+i = 0
+while i < len(arr):
+  # also can use total = total + arr[i]
+  total1 += arr[i] + " "
+  i += 1
+print(total1)
+

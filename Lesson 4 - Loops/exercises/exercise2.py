1c6b88ddc69a91ea2e21855b47439062a2fdf393
Madelyn Forrester <forrestermadelyn@gmail.com>
2020-07-23 18:02:48 +0000

    exercise

---
+++ Lesson 4 - Loops/exercises/exercise2.py
@@ -0,0 +1,28 @@
+"""
+WELCOME TO LOOPS EXERCISE 2
+
+BEFORE YOU DO ANY CODING COPY THE BELOW TEXT INTO THE .replit FILE
+
+language = "python3"
+run = "cd 'Lesson 4 - Loops'; cd exercises; clear; python3 exercise2.py"
+
+For examples, please refer to the example files
+
+Start with the base code below 
+
+GOAL: Create a menu that offers 
+
+Hint: 
+
+"""
+
+""" BASE CODE STARTS HERE - DO NOT CHANGE CODE BELOW """
+
+
+""" YOUR CODE STARTS HERE """
+
+
+
+""" This should print out:
+
+"""

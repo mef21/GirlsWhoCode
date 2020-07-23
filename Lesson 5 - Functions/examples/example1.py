52bf5de3f858ae92a6f4b88888c93e1737efbc09
Madelyn Forrester <forrestermadelyn@gmail.com>
2020-07-23 19:15:21 +0000

    exercises

---
+++ Lesson 5 - Functions/examples/example1.py
@@ -0,0 +1,24 @@
+"""
+WELCOME TO FUNCTION EXAMPLES 1
+
+BEFORE YOU DO ANY CODING COPY THE BELOW TEXT INTO THE .replit FILE
+
+language = "python3"
+run = "cd 'Lesson 5 - Functions'; cd examples; clear; python3 example1.py"
+
+"""
+
+def product(x,y): 
+  return x*y
+
+def divide(x,y):
+  return x/y
+
+def add(x,y):
+  return x + y
+
+def subtract(x,y):
+  return x - y
+
+while():
+  
---
+++ Lesson 5 - Functions/examples/example2.py
@@ -0,0 +1,30 @@
+"""
+WELCOME TO FUNCTION EXAMPLES 2
+
+BEFORE YOU DO ANY CODING COPY THE BELOW TEXT INTO THE .replit FILE
+
+language = "python3"
+run = "cd 'Lesson 5 - Functions'; cd examples; clear; python3 example2.py"
+
+"""
+def add_two(arr): 
+  for i in range(0, (len(arr))):
+    arr[i]= arr[i] + 2
+  return arr
+
+def count_as(arr):
+  count = 0 
+  for i in arr:
+    if (i == 'a'): 
+      count +=1
+  return count 
+
+def add_twoo(arr): 
+  for i in range(0, len(arr)): 
+    if(arr[i] > 10):
+      arr[i] += 2
+  return arr
+
+print(add_twoo([6, 8, 9, 10, 11, 15, 12, 50]))
+print(add_two([1,2,4,5]))
+print(count_as(['w', 'a', 'r', 'a', 'a']))

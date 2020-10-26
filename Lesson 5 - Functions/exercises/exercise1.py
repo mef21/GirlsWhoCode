11dd5e389d56320e160dba63d8fd19d4d9e0106b
michelleUMD <68123109+michelleUMD@users.noreply.github.com>
2020-10-25 21:12:56 -0400

    Added in an exercise for extra functions practice

--- Lesson 5 - Functions/exercises/exercise1.py
+++ Lesson 5 - Functions/exercises/exercise1.py
@@ -11,17 +11,54 @@ For examples, please refer to the example files
 Start with the base code below 
 
 GOAL: 
+Write two functions: count_down and count_up. 
+count_down takes in two integers and counts down from the first integer to the second integer. Then it prints "Happy New Year!"
+
+count_up takes in two integers and counts up from the first integer to the second integer. Then it prints "Take off!"
 
 HINT: 
+The skeleton to create a function is:
+def function_name (parameters):
+  Body of your function here
+
 """
-
-""" BASE CODE STARTS HERE - DO NOT CHANGE CODE BELOW """
-
 
 """ YOUR CODE STARTS HERE """
 
 
 
+""" BASE CODE STARTS HERE - DO NOT CHANGE CODE BELOW """
+print("count down!")
+count_down(10, 1)
+print("count up!")
+count_up(0, 10)
+
+
 """ This should print out:
+count down!
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+Happy New Year!
+count up!
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Take off!
 
 """
